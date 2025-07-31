8ff15bf18e151d14dd08c141d2dbe1c342e2e88e
tngo <tngoo16062001@gmail.com>
2025-08-01 00:03:20 +0700

    eslint error

--- src/types/mathtype.d.ts
+++ src/types/mathtype.d.ts
@@ -1,7 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type MathExpression = any;
-declare module '@wiris/mathtype-ckeditor5' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// This declaration file tells TypeScript about the module's structure
+declare module '@wiris/mathtype-ckeditor5/dist/index.js' {
   const MathType: any;
   export default MathType;
 }
+
+// You can remove your previous `type MathExpression = any;` line if it's no longer needed,
+// as it was part of a different error fix.
