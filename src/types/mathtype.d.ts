f6673e17daad5ad9b877637d13f3aa1ac69efc55
tngo <tngoo16062001@gmail.com>
2025-08-01 00:00:16 +0700

    eslint error

--- src/types/mathtype.d.ts
+++ src/types/mathtype.d.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type MathExpression = any;
 declare module '@wiris/mathtype-ckeditor5' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const MathType: any;
   export default MathType;
 }
